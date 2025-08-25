053b27f900340a8697a6e71985e409c4e08f3a57
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-08-25 12:17:44 +0530

    Admin Purchase

--- src/app/component/PurchaseOrders.jsx
+++ src/app/component/PurchaseOrders.jsx
@@ -1,12 +1,7 @@
-
 "use client";
 
 import { useState } from "react";
-import {
-  Box,
-  Typography,
-  Table,
-  TableBody,TableCell,TableContainer,TableHead,TableRow,Paper,IconButton,Modal,Divider,Button,} from "@mui/material";
+import {Box,Typography,Table,TableBody,TableCell,TableContainer,TableHead,TableRow,Paper,IconButton,Modal,Divider,Button,} from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
